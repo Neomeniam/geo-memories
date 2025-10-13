5b0cbd977f0d9c3788ea50d1093b524f581f28ca
Neomeniam <dnmfuture@gmail.com>
2025-10-13 12:56:20 +0800

    Ready for the park

--- GeoMemories/settings.py
+++ GeoMemories/settings.py
@@ -21,13 +21,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)30!p%@n_^x&fdk+t!f!_ir*-6c=m#2$3tm2=$je8h=pi&y5_t'
+# SECRET_KEY = 'django-insecure-)30!p%@n_^x&fdk+t!f!_ir*-6c=m#2$3tm2=$je8h=pi&y5_t'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+# 2025.10.13 SECRET_KEY changed for running on render
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+# 2025.10.13 DEBUG changed for running on render
+
 
 ALLOWED_HOSTS = []
-
+# 2025.10.13 ALLOWED_HOSTS changed for running on render
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
